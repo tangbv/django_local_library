eaac9d1d08feed46dfe27be8b621d628a8e25569
tangbv <tangbv@dukes.jmu.edu>
2025-04-01 09:55:08 -0400

    added enviorn to settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -15,11 +15,14 @@ from django.core.management.utils import get_random_secret_key
 import os
 import sys
 import dj_database_url
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+# Load environment variables
+env.read_env(str(BASE_DIR / ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
